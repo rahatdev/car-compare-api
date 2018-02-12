0243fb406445c01d10bf865ec7bf2df105b83b8d
rahat <rahat.forja@gmail.com>
2018-02-12 15:00:21 -0600

    cleared stock html, added initial home properties.

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  result: String;
+  zipcode: Number;
+  annualMilage: Number;
+  percentCity: Number;
+  percentHighway: Number;
 
-  constructor() { }
+  constructor(
+    //private _vehicleData: VehicleDataService
+  ) { }
 
   ngOnInit() {
   }
