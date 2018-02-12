82d42e1e7a4f7edbb3b9bcba7061d75c73ca2074
rahat <rahat.forja@gmail.com>
2018-02-12 15:04:34 -0600

    removed control details from home.

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -6,11 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  result: String;
-  zipcode: Number;
-  annualMilage: Number;
-  percentCity: Number;
-  percentHighway: Number;
+
 
   constructor(
     //private _vehicleData: VehicleDataService
