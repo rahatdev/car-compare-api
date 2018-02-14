9969a66538b27f7ff1b6a1e5fcb975a9090800ad
rahat <rahat.forja@gmail.com>
2018-02-13 22:32:22 -0600

    cool, functional js works in ts

--- client/src/app/components/vehicle/vehicle.component.ts
+++ client/src/app/components/vehicle/vehicle.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle.component.scss']
 })
 export class VehicleComponent implements OnInit {
+  
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  testfunct() {
+    let arr = [2, 3, 23, 34, 1, 4, 5, 4];
+
+    //arr.forEach(i => console.log(i +3));
+
+    arr.map(val => val * 3)
+       .filter(val => val % 2 === 0)
+       .forEach(val => console.log(val));
+
   }
 
 }
