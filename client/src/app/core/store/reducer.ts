873362757674a13c9a233e46300e48575ec8f425
rahat <rahat.forja@gmail.com>
2018-03-12 15:43:37 -0500

    updated reducer for general selection action state updates

--- client/src/app/core/store/reducer.ts
+++ client/src/app/core/store/reducer.ts
@@ -1,9 +1,34 @@
-import { IAppState } from ".";
+import { IAppState } from '.';
+import { UPDATE_LOCATION, UPDATE_MILESDRIVEN, UPDATE_PERCENTHIGHWAY } from '../../general-selection/general-selection.actions';
 
 const initialState: IAppState = {}
 
-export function reducer(state, action){
-    switch(action.type){
+function updateLocation(state: IAppState, action): IAppState {
+    return Object.assign({}, state, {
+        location: action.location
+    });
+}
+
+function updateMilesDriven(state: IAppState, action): IAppState {
+    return Object.assign({}, state, {
+        milesDriven: action.miles
+    });
+}
+
+function updatePercentHighway(state: IAppState, action): IAppState {
+    return Object.assign({}, state, {
+        percentHighway: action.percentHighway
+    });
+}
+
+export function reducer(state = initialState, action) {
+    switch (action.type) {
+        case UPDATE_LOCATION:
+            return updateLocation(state, action);
+        case UPDATE_MILESDRIVEN:
+            return updateMilesDriven(state, action);
+        case UPDATE_PERCENTHIGHWAY:
+            return updatePercentHighway(state, action);
         default:
             return state;
     }
